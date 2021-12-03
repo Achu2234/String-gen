ea9ff29ca100c023a7215ce0becff1f6c9782f4e
Miko <75154223+Achu2234@users.noreply.github.com>
2021-12-03 12:10:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# Credits to Calls Music
-# I'm Not A Bot
+# Credits to Achu biju
 
 import asyncio
 import tgcrypto
@@ -10,15 +9,12 @@ print("Made For")
 print("")
 print('''
  / ___|__ _| | |___|  \/  |_   _ ___(_) ___     |  _ \| |_   _ ___ 
-| |   / _` | | / __| |\/| | | | / __| |/ __|____| |_) | | | | / __|
-| |__| (_| | | \__ \ |  | | |_| \__ \ | (_|_____|  __/| | |_| \__ \.
+
  \____\__,_|_|_|___/_|  |_|\__,_|___/_|\___|    |_|   |_|\__,_|___/
 ''')
 print("")
 print('''
- /\  /  _  _ |  | _ | _  _ .__.._ _  \  //  |\/|    _o _ |_)| _.   _ ._| 
-/--\ \_(_)(_)|  |(/_|(/_(_||(_|| | |  \/ \_ |  ||_|_>|(_ |  |(_|\/(/_| o 
-                         _|                                     /        
+nothing                         
 ''')
 print("")
 print("Enter All Required Things To Generate String Session.")
